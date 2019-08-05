3dc8c65b8e89a5bae818c8ddfa06b2e3e8c0c82d
Eli Fawcett <30002105@student.toiohomai.ac.nz>
2019-08-05 15:45:29 +1200

    A:/

---
+++ Stacks and Queues/Stacks and Queues/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Stacks_and_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s1 = new Stack();
+            s1.Push("one");
+            s1.Push("house");
+            s1.Push(1);
+            s1.Push("$");
+            s1.Push("32");
+            s1.Push(16);
+            s1.Push(true);
+            s1.Push(false);
+            s1.Push(1.23);
+            s1.Push(61);
+            Console.WriteLine("count: "+ s1.Count);
+            Console.WriteLine("Peek: "+ s1.Peek());
+
+            if (s1.Contains(true))
+            {
+                Console.WriteLine("Deleting: "+ s1.Pop());
+                Console.WriteLine("Deleting: " + s1.Pop());
+                Console.WriteLine("Deleting: " + s1.Pop());
+            }
+            Console.WriteLine("Number of Objects in the Stack: "+ s1.Count);
+
+
+        }
+    }
+}
---
+++ Task2Stacks/Task2Stacks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Task2Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q1 = new Queue();
+            for (int i = 4; i > q1.Count; i++) {
+                q1.Enqueue("house");
+                q1.Enqueue("once");
+                q1.Enqueue(true);
+                q1.Enqueue(1);
+                q1.Enqueue(5.5);
+                q1.Enqueue(5.5);
+                q1.Enqueue(5.5);
+            }
+            Console.WriteLine("Number of elements in Queue: "+ q1.Count);
+
+        }
+    }
+}
